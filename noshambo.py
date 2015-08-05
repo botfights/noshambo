651c147a0f59e095b32e8409d311f5772719f6dd
Colin M. Saunders <colins@surveymonkey.com>
2015-08-05 11:58:28 -0700

    broken

--- noshambo.py
+++ noshambo.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python
 
-# roshambono.py -- console harness
-
-HELP = '''\
-usage:
-
-    To play a game to 1000 between p_rock and p_random:
-
-        $ python roshambono.py play 1000 p_rock p_random
-
-    To play a round robin tourney for 100 games to 1000 between p_rock and p_paper and p_random: 
-
-        $ python roshambono.py tourney 100 1000 p_rock p_paper p_random
-'''
+# noshambo.py -- console harness
 
 import sys
 import imp
@@ -33,6 +21,7 @@ SCISSORS    = 3
 BEATS       = [0,3,1,2]
 BEAT_BY     = [0,2,3,1]
 
+
 def get_play(f_get_play,state,catch_exceptions) :
     play = 0
     try :
@@ -47,6 +36,7 @@ def get_play(f_get_play,state,catch_exceptions) :
         play = random.randint(1,3)
     logging.debug('LOG_PLAY\t%d\t%d\t%s' % (state,play,str(f_get_play)))
     return play
+
 
 def play_game(race_to,f_get_play_a,f_get_play_b,catch_exceptions) :
     wins = [0,0]
@@ -91,42 +81,6 @@ def play_game(race_to,f_get_play_a,f_get_play_b,catch_exceptions) :
         if wins[1] == race_to :
             return 1
 
-def split_playername(playername) :
-    parts = playername.split(':')
-    if 1 == len(parts) :
-        return (parts[0],parts[0],'player','get_play')
-    if 2 == len(parts) :
-        return (parts[0],parts[1],'player','get_play')
-    if 3 == len(parts) :
-        return (parts[0],parts[1],parts[2],'get_play')
-    if 4 == len(parts) :
-        return (parts[0],parts[1],parts[2],parts[3])
-    raise Exception('i don\'t know how to parse "%s"' % playername)
-
-def make_player(playername,catch_exceptions) :
-    name,path,modulename,attr = split_playername(playername)
-    fp = pathname = description = m = None
-    try :
-        fp,pathname,description = imp.find_module(modulename,[path,])
-    except :
-        if not catch_exceptions:
-            raise
-        logging.warn('caught exception "%s" finding module %s' % (sys.exc_info()[1],modulename))
-    try :
-        if fp :
-            m = imp.load_module(playername,fp,pathname,description)
-    except :
-        if not catch_exceptions:
-            raise
-        logging.warn('caught exception "%s" importing %s' % (sys.exc_info()[1],playername))
-    finally :
-        if fp :
-            fp.close()
-    if None == m :
-        return None
-    f = getattr(m,attr)
-    return f
-
 def play_tourney(t,n,playernames) :
     scores = {}
     players = []
@@ -149,41 +103,164 @@ def play_tourney(t,n,playernames) :
     logging.info('STATUS\t%.2f\t\t%s' % (100.0,','.join(map(lambda i : '%s:%s' % (playernames[i],scores[i]),range(len(players))))))
     return -1
 
-def main(argv) :
-    if 1 == len(argv) :
-        print HELP
+
+class Player():
+    pass
+
+
+def call_player(player, args, default):
+    global g_catch_exceptions
+    result = default
+    start = time.clock()
+    try:
+        result = player.play(*args)
+    except KeyboardInterrupt:
+        raise
+    except:
+        logging.warn('caught exception "%s" calling %s (%s)'
+                     % (sys.exc_info()[1], player.player_id, player.playername))
+        if not g_catch_exceptions:
+            raise
+    elapsed = time.clock() - start
+    player.elapsed += elapsed
+    player.calls += 1
+    return result
+
+
+def make_player(player_id, dirname):
+    global g_catch_exceptions
+    m = None
+    try:
+        name = 'bot'
+        (f, filename, data) = imp.find_module(name, [dirname, ])
+        m = imp.load_module(name, f, filename, data)
+    except:
+        logging.error('caught exception "%s" loading %s' % \
+                      (sys.exc_info()[1], dirname))
+        if not g_catch_exceptions:
+            raise
+    p = Player()
+    p.player_id = player_id
+    p.playername = dirname
+    z = p.playername.rfind('/')
+    if -1 != z:
+        p.playername = p.playername[z + 1:]
+    p.play = None
+    if None == m or not hasattr(m, 'play'):
+        logging.error('%s has no function "play"; ignoring ...' % dirname)
+    else:
+        p.play = getattr(m, 'play')
+    p.elapsed = 0.0
+    p.calls = 0
+    p.get_play = lambda x: call_player(p, (p.player_id, p.hand, x), 'F')
+    return p
+
+
+def usage():
+    print('''\
+noshambo! see: http://github.com/botfights/noshambo for dox
+
+usage:
+
+    $ python noshambo.py <command> [<option> ...] [<arg> ...]
+
+commands:
+
+    game [<player1>] [<player2>] ...
+
+                        play a single game between players
+
+    tournament [<player1>] [<player2>] ...
+
+                        play a tournament between players
+options:
+
+    -h, --help                      show this help
+    --seed=<s>                      set seed for random number generator
+    --catch-exceptions=<off|on>     catch and log exceptions
+    --race-to=<n>                   set number of hands to play per game (defaults to 100)
+    --num-games=<n>                 set number of games for tournament (defaults to 100)
+
+    --log-level=<n>                 set log level (10 debug, 20 info, 40 error)
+''')
+
+
+def main(argv):
+    if 1 > len(argv):
+        usage()
         sys.exit()
+    command = argv[0]
+    try:
+        opts, args = getopt.getopt(argv[1:], "h", [
+                                                    "help",
+                                                    "seed=",
+                                                    "catch-exceptions=",
+                                                    "num-games=",
+                                                    "race-to=",
+                                                    "log-level=",
+                                                    ])
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+        sys.exit(1)
+    seed = time.time()
+    num_games = 100
+    race_to = 100
+    log_level = logging.DEBUG
+    global g_catch_exceptions
+    g_catch_exceptions = False
+    for o, a in opts:
+        if 0:
+            pass
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("--seed", ):
+            seed = a
+        elif o in ("--num-games", ):
+            num_games = int(a)
+        elif o in ("--race-to", ):
+            race_to = int(a)
+        elif o in ("--log-level", ):
+            log_level = int(a)
+        elif o in ("--catch-exceptions", ):
+            g_catch_exceptions = 'off' != a
+        else:
+            raise Exception("unhandled option")
+    random.seed(seed)
 
-    c = argv[1]
-
-    if 0 :
+    if 0:
         pass
 
-    elif 'help' == c :
-        print HELP
+    elif 'game' == command:
+        logging.basicConfig(level=log_level, format='%(message)s', 
+                        stream=sys.stdout)
+        players = []
+        for player_id, playername in enumerate(args):
+            if verify_players:
+                if 0 != verify_player(playername):
+                    continue
+            players.append(make_player(chr(ord('a') + player_id), playername))
+        winner = play_game(players)
         sys.exit()
 
-    elif 'play' == c :
-        logging.basicConfig(level=logging.DEBUG,format='%(message)s',stream=sys.stdout)
-        n = int(sys.argv[2])
-        a_player = make_player(argv[3],False)
-        b_player = make_player(argv[4],False)
-        x = play_game(n,a_player,b_player,False)
-        return x
-  
-    elif 'tourney' == c :
-        logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)-7s %(message)s',stream=sys.stdout)
-        t = int(sys.argv[2])
-        n = int(sys.argv[3])
-        playernames = sys.argv[4:]
-        x = play_tourney(t,n,playernames)
-        return x
-    
-    else :
-        logging.error('i don\'t know how to "%s". look at the source' % c)
-        print HELP
+    elif 'tournament' == command:
+        logging.basicConfig(level=log_level, format='%(message)s', 
+                        stream=sys.stdout)
+        players = []
+        for player_id, playername in enumerate(args):
+            if verify_players:
+                if 0 != verify_player(playername):
+                    continue
+            players.append(make_player(chr(ord('a') + player_id), playername))
+        play_tournament(num_games, players)
         sys.exit()
 
-if __name__ == '__main__' :
-    main(sys.argv)
+    else:
+        print 'i don\'t know how to "%s".' % command
+        usage()
+        sys.exit()
 
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
